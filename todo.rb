c1a990d654ad3bcc2f44c7b5b70a6954c87e2fc3
　森岡　大智 <morioka-d@moriokataichinoMacBook-Pro.local>
2026-05-12 13:24:54 +0900

    3-2 メソッドに切り出す

--- todo.rb
+++ todo.rb
@@ -1,10 +1,16 @@
-command = ARGV[0]
-task = ARGV[1]
+def list_tasks
+  puts "牛乳を買う"
+  puts "rubyを勉強する"
+end
 
-case
-when command == "add"
-    puts "追加しました: #{task}"
-when command == "list"
-    puts "牛乳を買う"
-    puts "rubyを勉強する"
+def add_task
+  task = ARGV[1]
+  puts "追加しました: #{task}"
+end
+
+case ARGV[0]
+when "list"
+  list_tasks
+when "add"
+  add_task
 end
