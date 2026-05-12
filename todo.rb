546faedb8950f5d267aecc46f97e037a7354ca9d
　森岡　大智 <morioka-d@moriokataichinoMacBook-Pro.local>
2026-05-12 11:48:26 +0900

    3-1 サブコマンドで引数を受け取る

--- todo.rb
+++ todo.rb
@@ -1,4 +1,10 @@
-if ARGV[0] == "list"
+command = ARGV[0]
+task = ARGV[1]
+
+case
+when command == "add"
+    puts "追加しました: #{task}"
+when command == "list"
     puts "牛乳を買う"
     puts "rubyを勉強する"
 end
